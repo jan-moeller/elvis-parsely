0ead5fbc769c03f13850dfca839bc14425eba527
Jan Möller <drag-on@gmx.de>
2024-12-10 23:34:58 +0100

    Add basic cmake infrastructure

---
+++ include/parsely/parsely.hpp
@@ -0,0 +1,9 @@
+//
+// Elvis Parsely
+// Copyright (c) 2024 Jan Möller.
+//
+
+#ifndef PARSELY_HPP
+#define PARSELY_HPP
+
+#endif //PARSELY_HPP
---
+++ test/test.cpp
@@ -0,0 +1,4 @@
+//
+// Elvis Parsely
+// Copyright (c) 2024 Jan Möller.
+//
